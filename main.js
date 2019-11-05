980eb6c52b11e2b4be54d4a0366528b33fbab26a
Kiril Vasilev <kvasilev@vmware.com>
2019-11-05 14:32:47 +0200

    connected all components

--- main.js
+++ main.js
@@ -1,66 +1,22 @@
 // Modules to control application life and create native browser window
 const path = require('path');
-const { app, BrowserWindow, Menu, MenuItem } = require('electron');
+const { app } = require('electron');
 const { menubar } = require('menubar');
-const DriveService = require('./providers/drive-service');
-const DatabaseService = require('./lib/database/mssql-service');
-const getLogger = require('./common/logger');
-
-const logger = getLogger('drive-service');
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
-async function createWindow() {
-    const driverService = new DriveService();
-    // await driverService.upload('./main.js');
-    const abaut = await driverService.getStorageQuota();
-    console.dir(abaut);
-    // driverService.downloadFile('13wIbKf6fAq3-RTAYVPD2zMJ6OkKPxbV_');
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-        },
-    });
-
-    // and load the index.html of the app.
-    mainWindow.loadFile('index.html');
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 
 const mb = menubar({
     browserWindow: {
         transparent: true,
         // resizable: false,
-        width: 330,
+        width: 360,
         height: 330,
         webPreferences: {
             nodeIntegration: true,
+            // preload: path.join(__dirname, 'preload.js'),
+            nodeIntegrationInWorker: true,
         },
     },
-    icon: path.join(__dirname, 'assets/icons/icon_32x32.png'),
+    icon: path.join(__dirname, 'assets/icons/icon_20x20.png'),
 });
-
-// mb.on('ready', () => {
-//   console.log('Menubar app is ready.');
-// });
 
 mb.on('ready', async () => {
     // createWindow();
@@ -68,37 +24,10 @@ mb.on('ready', async () => {
 mb.on('after-create-window', async () => {
     mb.window.webContents.openDevTools({ mode: 'detach' });
 });
-mb.on('right-click', () => {
-    // eslint-disable-next-line no-console
-    console.log('right click');
-    const menu = new Menu();
-    menu.append(new MenuItem({
-        label: 'Resume',
-        click() {
-            console.log('resume clicked');
-        },
-    }));
-    menu.append(new MenuItem({ type: 'separator' }));
-    menu.append(new MenuItem({
-        label: 'Pause',
-        click() {
-            console.log('item 2 clicked');
-        },
-    }));
-    menu.popup();
-});
+
 // Quit when all windows are closed.
 mb.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') app.quit();
 });
-
-mb.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) createWindow();
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,16 +1,69 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
-const DriveService = require('./providers/drive-service');
+/* eslint-disable no-unused-vars */
+const { EventEmitter } = require('events');
+const DriveService = require('./lib/providers/drive-service');
+const BackupService = require('./lib/services/backup-service');
 
-const driverService = new DriveService();
+const driveService = new DriveService();
+const backupService = new BackupService();
+const deleteEmitter = new EventEmitter();
 
+function loadBackupsList() {
+    const backupsList = document.getElementById('backups-list');
+    backupsList.innerHTML = '';
+    driveService.listFiles().then((backups) => {
+        backups.forEach((backup) => {
+            const li = document.createElement('li');
+            li.classList.add('list-group-item');
+            const div = document.createElement('div');
+            div.classList.add('custom-control');
+            div.classList.add('custom-checkbox');
+            const input = document.createElement('input');
+            input.classList.add('custom-control-input');
+            input.setAttribute('type', 'checkbox');
+            input.setAttribute('id', backup.id);
+            input.setAttribute('name', backup.name);
+            const label = document.createElement('label');
+            label.classList.add('custom-control-label');
+            label.setAttribute('for', backup.id);
+            label.innerHTML = backup.name;
+            div.appendChild(input);
+            div.appendChild(label);
+            li.appendChild(div);
+            backupsList.appendChild(li);
+        });
+    });
+}
+function loadInfo() {
+    const backupsCount = document.getElementById('backups-count');
+    const lastBackup = document.getElementById('last-backup');
+    const freeSpace = document.getElementById('free-space');
+    driveService.listFiles().then((files) => {
+        backupsCount.innerText = `Available backups: ${files.length}`;
+        lastBackup.innerText = `Last backup: ${files.length ? files[0].createdTime.format('DD.MM.YY HH.mm') : ''}`;
+    });
+    driveService.getStorageQuota().then((quota) => {
+        freeSpace.innerText = `Available space: ${quota.usage}/${quota.limit}`;
+    });
+}
+
+deleteEmitter.on('delete', () => {
+    const files = document.querySelectorAll('.custom-control-input');
+    if (files.length) {
+        files.forEach((file) => {
+            if (file.checked) {
+                driveService.delete(file.id).then(() => {
+                    setTimeout(loadBackupsList, 2000);
+                });
+            }
+        });
+    }
+});
 
 document.querySelectorAll('.nav-link').forEach((element) => {
     element.addEventListener('click', () => {
+        if (element.classList.contains('active')) {
+            return;
+        }
         document.querySelectorAll('.nav-link').forEach((el) => {
             el.classList.remove('active');
         });
@@ -25,36 +78,49 @@ document.querySelectorAll('.nav-link').forEach((element) => {
         });
         element.classList.add('active');
         switch (element.innerHTML.toLowerCase()) {
-        case 'info':
+        case 'info': loadInfo();
             break;
-        case 'backups': {
-            const backupsList = document.getElementById('backups-list');
-            backupsList.innerHTML = '';
-            driverService.listFiles().then((backups) => {
-                backups.forEach((backup) => {
-                    const li = document.createElement('li');
-                    li.classList.add('list-group-item');
-                    const div = document.createElement('div');
-                    div.classList.add('custom-control');
-                    div.classList.add('custom-checkbox');
-                    const input = document.createElement('input');
-                    input.classList.add('custom-control-input');
-                    input.setAttribute('type', 'checkbox');
-                    input.setAttribute('id', backup.id);
-                    const label = document.createElement('label');
-                    label.classList.add('custom-control-label');
-                    label.setAttribute('for', backup.id);
-                    label.innerHTML = backup.name;
-                    div.appendChild(input);
-                    div.appendChild(label);
-                    li.appendChild(div);
-                    backupsList.appendChild(li);
-                });
-            });
-        }
+        case 'backups': loadBackupsList();
             break;
         default:
             break;
         }
     });
 });
+
+function backupToDrive() {
+    const progressEmitter = new EventEmitter();
+    const progressDiv = document.querySelector('div.progress.d-none');
+    const backupButton = document.getElementById('backup-button');
+
+    backupButton.setAttribute('disabled', 'true');
+    progressEmitter.on('progress', (progress) => {
+        const progressBar = document.getElementById('upload-progress');
+
+        if (progressDiv.classList.contains('d-none')) {
+            progressDiv.classList.remove('d-none');
+        }
+        progressBar.setAttribute('aria-valuenow', progress);
+        progressBar.style.cssText = `width: ${progress}%`;
+    });
+    backupService.backup(progressEmitter).then(() => {
+        loadInfo();
+        progressDiv.classList.add('d-none');
+        backupButton.removeAttribute('disabled');
+    });
+}
+
+function downloadBackup() {
+    const files = document.querySelectorAll('.custom-control-input');
+    files.forEach((file) => {
+        if (file.checked) {
+            driveService.downloadFile(file.id, file.name);
+        }
+    });
+}
+
+function confirmDelete() {
+    deleteEmitter.emit('delete');
+}
+
+loadInfo();
